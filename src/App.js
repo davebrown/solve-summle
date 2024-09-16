a54abeff1d7bf6bf89552d0ef4ce8109653fbaf9
Dave Brown <dave@moonspider.com>
2024-09-15 17:05:41 -0700

    Basic functional UX.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const myWorker = new Worker(new URL('./worker.js', import.meta.url));
+const isInt = Number.isInteger;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.runSolve = this.runSolve.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.setResult.bind(this);
+    this.state = {
+      target: '',
+      inputs: [],
+      solution:  "NA",
+      valid: false
+    };
+    myWorker.onmessage = function(event) {
+      this.setResult(event.data);
+    }.bind(this);
+  }
+
+  setResult(result) {
+    var { target, inputs } = this.state;
+    console.log("setResult(" + result + ")");
+    this.setState({
+      target: target,
+      inputs: inputs,
+      solution: result
+    });
+    //myWorker.terminate();
+  }
+  runSolve() {
+    console.log("solve clicked: target=" + this.state.target + ", inputs=" + this.state.inputs);
+    myWorker.postMessage(this.state);
+  }
+
+  
+  handleChange(event) {
+    var { target, inputs, solution } = this.state;
+    const id = event.target.id;
+    console.log("handleChange: " + id + "->'" + event.target.value + '\'');
+    if (id === "target") {
+      var value = parseInt(event.target.value);
+      if (isNaN(value)) {
+        value = '';
+      }
+      target = value;
+    } else if (id === "numbers") {
+      inputs = event.target.value.split(/[\s,]+/).filter(Boolean).map(Number);
+      console.log('inputs changed ' + inputs);
+    }
+    this.setState({
+      target: target,
+      inputs: inputs,
+      solution: solution
+    });
+  }
+
+  render() {
+    const { target, inputs, solution } = this.state;
+    const valid = Number.isInteger(target) && inputs.length === 6 && inputs.every(Number.isInteger);
+    return (
+      <div className="App">
+        <header className="App-header">
+          target: <input id="target" value={isNaN(target) ? '' : target} onChange={this.handleChange}/>
+          numbers: <input id="numbers" onChange={this.handleChange}/>
+          <button id="solve" onClick={this.runSolve} disabled={!valid}>Solve</button>
+          Target: {target}<br/> Inputs: {inputs.join(",")}<br/>
+      Solution: {solution}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/worker.js
@@ -0,0 +1,52 @@
+// cribbed from here: https://gist.github.com/mvladic/9b2f2025a8c54aca78649ce1776600b6
+
+const op = [['+', (a, b) => a + b], ['-', (a, b) => a - b], ['*', (a, b) => a * b], ['/', (a, b) => a / b]];
+
+let bestSolution;
+
+let target = 0;
+let inputs = [];
+
+
+onmessage = function(event) {
+  target = event.data.target;
+  inputs = event.data.inputs;
+  //const result = event.data * 2; // Example computation
+  console.log("worker invoked: target=" + target + " inputs=" + inputs);
+  solve(target, inputs, []);
+  var returnValue = "DID NOT SOLVE";
+  if (bestSolution) {
+    returnValue = bestSolution.join(" | ");
+  }
+  postMessage(returnValue);
+};
+
+function solve(target, nums, solution) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (j !== i) {
+        for (let k = 0; k < op.length; k++) {
+          const res = op[k][1](nums[i], nums[j]);
+          if (res <= 0) break;
+          if (Number.isInteger(res)) {
+            const newSolution = [...solution, `${nums[i]} ${op[k][0]} ${nums[j]} = ${res}`];
+            if (res === target) {
+              if (/*true*/!bestSolution || newSolution.length < bestSolution.length) {
+                bestSolution = newSolution;
+                console.log(newSolution);
+              }
+            } else {
+              if (newSolution.length < 5) {
+                const newNums = nums.slice();
+                newNums.splice(i, 1);
+                newNums.splice(j - (i < j ? 1 : 0), 1);
+                newNums.push(res);
+                solve(target, newNums, newSolution);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
